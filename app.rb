41ca28fe7d9fc851f427d59ac52d6e4efe2b55b4
Pedro Mamede <pedro.mamede@gmail.com>
2021-09-23 12:17:46 -0300

    testing rekognition sdk

--- app.rb
+++ app.rb
@@ -1,5 +1,10 @@
+require 'dotenv'
+Dotenv.load
 require 'sinatra'
 require 'sinatra/reloader' if development?
+require './lib/hashtager.rb'
+
+Hashtager.test
 
 get "/" do
   "Hello World!"
--- lib/hashtager.rb
+++ lib/hashtager.rb
@@ -1,24 +1,31 @@
-require 'aws/rekognition'
-require 'dotenv'
-Dotenv.load
+require 'aws-sdk-rekognition'
 
 class Hashtager
-  client = Aws::Rekognition::Client.new(
-    region:  ENV[AWS_REGION] || 'us-east-1',
-    credentials: { access_key_id: ENV[AWS_ACCESS_KEY_ID], secret_access_key: ENV[AWS_SECRET_ACCESS_KEY]}
-  )
 
-  photo = 'photo.jpg'
-  path = File.expand_path(photo)
-  file = File.read(path)
+  def self.test
+    client = Aws::Rekognition::Client.new(
+      region:  ENV['AWS_REGION'] || 'us-east-1',
+      access_key_id: ENV['AWS_ACCESS_KEY_ID'], 
+      secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
+    )
 
-  resp = client.detect_labels({
-    image: {
-      bytes: file
-    }, 
-    max_labels: 30, 
-    min_confidence: 70, 
-  })
+    photo = 'photo.jpg'
+    path = File.expand_path(photo)
+    file = File.read(path)
 
-  
+    resp = client.detect_labels({
+      image: {
+        bytes: file
+      }, 
+      max_labels: 30, 
+      min_confidence: 70, 
+    })
+
+    resp.labels.each do |label|
+      p '========='
+      p label.name
+      p label.confidence
+      p '========='
+    end
+  end
 end
